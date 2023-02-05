7b17a19fba2e75956f81c88b8bec9cfcd009f3a4
Ahmet Tarik Kazan <ahmettarkazan@gmail.com>
2023-02-05 23:37:50 +0300

    create ui

---
+++ app/src/main/java/com/example/lislearnapp/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -0,0 +1,14 @@
+package com.example.lislearnapp.feature_note.presentation.add_edit_note
+
+import androidx.compose.ui.focus.FocusState
+import com.example.lislearnapp.feature_note.domain.util.NoteOrder
+
+sealed class AddEditNoteEvent {
+
+    data class EnteredTitle(val value: String) : AddEditNoteEvent()
+    data class ChangeTitleFocused(val focusState: FocusState) : AddEditNoteEvent()
+    data class EnteredContent(val value: String): AddEditNoteEvent()
+    data class ChangeContentFocused(val focusState: FocusState) : AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
+    object SaveNote : AddEditNoteEvent()
+}
---
+++ app/src/main/java/com/example/lislearnapp/feature_note/presentation/notes/components/NoteItem.kt
@@ -0,0 +1,67 @@
+package com.example.lislearnapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.InspectableModifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.lislearnapp.feature_note.domain.model.Note
+import com.example.lislearnapp.feature_note.presentation.Greeting
+import com.example.lislearnapp.ui.theme.LisLearnAppTheme
+import java.util.Date
+
+@Composable
+fun NoteItem(
+    note: Note,
+    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 10.dp,
+    cutCornerSize: Dp = 30.dp,
+    onDeleteClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.
+                fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 32.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.h6,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(Modifier.height(8.dp))
+
+            Text(
+                text = note.content,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 10,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
+        IconButton(
+            onClick = onDeleteClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete Note")
+        }
+    }
+}
